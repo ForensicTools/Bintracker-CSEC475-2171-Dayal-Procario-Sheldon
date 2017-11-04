657a887513fb470f3863fee0c365b66286f12210
Pranat Dayal <pranatdayal@blap.local>
2017-11-04 10:27:24 -0400

    Created config file for BRO installation

---
+++ config.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#config script for bro and cuckoo
+
+red='\033[0;31m'
+green='\033[0;32m'
+nc='\033[0m'
+
+function dependencies
+{
+    echo -e "${green}[*] Installing required dependencies${nc}"
+    apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev   python-dev swig zlib1g-dev
+    if [[ $? > 0]]
+    then
+        echo -e "${red}[!!] Could not install dependencies${nc}"
+        exit
+    else
+        echo -e "${green}[+] Dependencies installed!${nc}"
+    fi
+}
+
+function downloadbro
+{
+    cd ~
+    echo -e "${green}[*] Cloning BRO IDS${nc}"
+    git clone https://github.com/bro/bro.git
+    if [[ $? > 0]]
+    then
+        echo -e "${red}[!!] Could not clone BRO${nc}"
+        exit
+    else
+        echo -e "${green}[+] BRO cloned! ${nc}"
+    fi
+
+}
+
+function installbro
+{
+    cd bro
+    ./configure && make && make install
+
+}
+
+
+function main
+{
+    echo -e "${green}[*]Configuring BRO IDS${nc}"
+    $dependencies
+    $downloadbro
+    $installbro
+
+}
+
+main
