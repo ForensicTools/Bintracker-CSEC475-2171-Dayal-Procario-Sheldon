30bfe6cfa1bee509992b6dc4162af06351af8ab1
Pranat Dayal <pranatdayal@blap.local>
2017-11-12 18:16:31 -0500

    Updated

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ nc='\033[0m'
 function dependencies
 {
     echo -e "${green}[*] Installing required dependencies${nc}"
-    apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev
+    apt-get install bison cmake flex g++ gdb make libmagic-dev libpcap-dev libgeoip-dev libssl-dev python-dev swig2.0 zlib1g-dev
     if ( $? > 0)
     then
         echo -e "${red}[!!] Could not install dependencies${nc}"
@@ -22,7 +22,7 @@ function downloadbro
 {
     cd ~
     echo -e "${green}[*] Cloning BRO IDS${nc}"
-    git clone https://github.com/bro/bro.git
+    git clone --reverse git://git.bro.org/bro
     if ( $? > 0)
     then
         echo -e "${red}[!!] Could not clone BRO${nc}"
