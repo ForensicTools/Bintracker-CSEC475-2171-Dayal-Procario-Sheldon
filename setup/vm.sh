4e8a251793663e9fd0504640a130fd51100e3dff
Pranat Dayal <pranatdayal@pxd5104.student.rit.edu>
2017-11-13 12:22:48 -0500

    Yara/ssdeep

--- setup/vm.sh
+++ setup/vm.sh
@@ -5,23 +5,19 @@ green='\033[0;32m'
 nc='\033[0m'
 
 
-if [$?>0]
-then
-    echo -e "${red}[!]Downlaod FAILED${nc}"
-else
-    echo -e "${green}[*]Download complete${nc}"
-fi
+
 VBoxManage createvm --name "Windowscuckoo" --register
 
 VBoxManage modifyvm "Windowscuckoo" --memory 256 --acpi off --boot1 dvd
 VBoxManage modifyvm "Windowscuckoo" --nic1 hostonly --hostonlyadapter1 vboxnet0
 VBoxManage modifyvm "Windowscuckoo" --ostype WindowsXP
 
-mkdir /srv/vms/
+mkdir /srv/vms
 VBoxManage createhd --filename /srv/vms/windowscuckoo.vdi --size 10000
 VBoxManage storagectl "Windowscuckoo" --name "IDE Controller" --add ide
 VBoxManage storageattach "Windowscuckoo" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium /srv/vms/windowscuckoo.vd1
 
+mkdir /srv/isos
 echo -e "${green}[*]Downloading Windows XP ISO${nc}"
 wget -o /srv/isos/windowscuckoo.iso http://pcriver1.com/download/pcriver.com_Windows_XP_Pro_SP3_32_bit.iso
 
