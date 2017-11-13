c7b240efd24332f64038352efb48bd8cc3e6e694
Pranat Dayal <pranatdayal@pxd5104.student.rit.edu>
2017-11-13 12:14:14 -0500

    Yara/ssdeep

--- setup/cuckoo.sh
+++ setup/cuckoo.sh
@@ -11,7 +11,9 @@ nc='\033[0m'
 function dependencies
 {
     echo -e "${green}[*]Installing cuckoo dependencies${nc}"
-    apt-get install python python-pip python-dev libffi-dev libssl-dev python-virtualenv python-setuptools libjpeg-dev zlib1g-dev swig mongodb postgresql libpq-dev qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils python-libvirt
+    apt-get install git python python-pip python-dev libffi-dev libssl-dev python-virtualenv python-setuptools libjpeg-dev zlib1g-dev swig mongodb postgresql libpq-dev qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils python-libvirt
+    apt-get install python python-sqlalchemy python-bson python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-pefile bridge-utils python-pyrex
+    pip install jinja2 pymongo bottle pefile cybox maec django chardet
 
     apt-get install tcpdump apparmor-utils
     aa-disable /usr/sbin/tcpdump
@@ -19,7 +21,42 @@ function dependencies
     apt-get install libcap2-bin
     setcap cap_net_raw.cap_net_admin=eip/usr/sbin/tcpdump
 
-    echo -e "${green}[+]Dependencies installed ${nc}"
+    #ssdeep
+    wget https://sourceforge.net/projects/ssdeep/files/ssdeep-2.12/ssdeep-2.12.tar.gz
+    tar xvzf ssdeep-2.12.tar.gz
+    cd ssdeep-2.12/
+    ./configure && make && make install
+    git clone https://github.com/kbandla/pydeep
+    cd pydeep
+    python setup.py build
+    python setup.py install
+
+    #yara
+     wget https://yara-project.googlecode.com/files/yara-1.7.tar.gz
+     tar xvzf yara-1.7.tar.gz
+     cd yara-1.7/
+     ./configure && make && make install
+     echo "/usr/local/lib" >> /etc/ld.so.conf
+     ldconfig
+
+     wget https://yara-project.googlecode.com/files/yara-python-1.7.tar.gz
+     tar xvzf yara-python-1.7.tar.gz
+     cd yara-python-1.7/
+     python setup.py build
+     python setup.py install
+
+     wget https://distorm.googlecode.com/files/distorm3.zip
+     unzip distorm3.zip
+     cd distorm3/
+     python setup.py build
+     python setup.py install
+
+     wget https://volatility.googlecode.com/files/volatility-2.3.1.tar.gz
+     tar xvzf volatility-2.3.1.tar.g
+     cd volatility-2.3.1/
+     python setup.py build
+     python setup.py install
+     echo -e "${green}[+]Dependencies installed ${nc}"
 
 }
 #install virtual box
@@ -35,21 +72,19 @@ function vbox
     echo -e "${green}[+]VirtualBox installed ${nc}"
 }
 #add user
-function user_add
-{
-    echo -e "${green}[*]Add Cuckoo user${nc}"
-    adduser cuckoo
-    usermod -a -G vboxusers cuckoo
-    usermod -a -G libvirtd cuckoo
-    echo -e "${green}[+]User added${nc}"
-
-}
 
 function install
 {
-    pip install -U pip setuptools
-    pip install -U cuckoo
+    adduser cuckoo
+    usermod -G vboxusers cuckoo
+    gitclone git://github.com/cuckoo.cuckoo.git
+}
 
+function cuckoo_config
+{
+    VBoxManage hostonlyif create
+    ip link set vboxnet0 up
+    ip addr add 192.168.56.1/24 dev vboxnet0
 }
 
 function main
@@ -59,6 +94,7 @@ function main
     vbox
     user_add
     install
+    cuckoo_config
     echo -e "${green}[+]Cuckoo installed${nc}"
 }
 
