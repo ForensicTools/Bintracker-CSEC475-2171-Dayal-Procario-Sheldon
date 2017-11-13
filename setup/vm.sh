b2f47771623c753f589fd5f47b93cde15201f775
Pranat Dayal <pranatdayal@pxd5104.student.rit.edu>
2017-11-13 12:24:54 -0500

    Yara/ssdeep

--- setup/vm.sh
+++ setup/vm.sh
@@ -4,7 +4,13 @@ red='\033[0;31m'
 green='\033[0;32m'
 nc='\033[0m'
 
+function download
+{
+    mkdir /srv/isos
+    echo -e "${green}[*]Downloading Windows XP ISO${nc}"
+    wget -o /srv/isos/windowscuckoo.iso http://pcriver1.com/download/pcriver.com_Windows_XP_Pro_SP3_32_bit.iso
 
+}
 
 VBoxManage createvm --name "Windowscuckoo" --register
 
@@ -17,10 +23,7 @@ VBoxManage createhd --filename /srv/vms/windowscuckoo.vdi --size 10000
 VBoxManage storagectl "Windowscuckoo" --name "IDE Controller" --add ide
 VBoxManage storageattach "Windowscuckoo" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium /srv/vms/windowscuckoo.vd1
 
-mkdir /srv/isos
-echo -e "${green}[*]Downloading Windows XP ISO${nc}"
-wget -o /srv/isos/windowscuckoo.iso http://pcriver1.com/download/pcriver.com_Windows_XP_Pro_SP3_32_bit.iso
-
+download
 
 VBoxManage storageattach "Windowscuckoo" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium /srv/vms/windowscuckoo.iso
 echo -e  "${green}[*]Running Windows VM${nc}"
