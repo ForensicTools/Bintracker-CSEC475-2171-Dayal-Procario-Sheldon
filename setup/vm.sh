90f9f1ed5c6133ede7349d5172c52956918fddb4
Pranat Dayal <pranatdayal@pxd5104.student.rit.edu>
2017-11-13 12:56:32 -0500

    vm fixed

--- setup/vm.sh
+++ setup/vm.sh
@@ -8,26 +8,26 @@ function download
 {
     mkdir /srv/isos
     echo -e "${green}[*]Downloading Windows XP ISO${nc}"
-    wget -o /srv/isos/windowscuckoo.iso http://pcriver1.com/download/pcriver.com_Windows_XP_Pro_SP3_32_bit.iso
+    wget -o /srv/isos/cuckoo1.iso http://pcriver1.com/download/pcriver.com_Windows_XP_Pro_SP3_32_bit.iso
 
 }
 
-VBoxManage createvm --name "Windowscuckoo" --register
+VBoxManage createvm --name "cuckoo1" --register
 
-VBoxManage modifyvm "Windowscuckoo" --memory 256 --acpi off --boot1 dvd
-VBoxManage modifyvm "Windowscuckoo" --nic1 hostonly --hostonlyadapter1 vboxnet0
-VBoxManage modifyvm "Windowscuckoo" --ostype WindowsXP
+VBoxManage modifyvm "cuckoo1" --memory 256 --acpi off --boot1 dvd
+VBoxManage modifyvm "cuckoo1" --nic1 hostonly --hostonlyadapter1 vboxnet0
+VBoxManage modifyvm "cuckoo1" --ostype WindowsXP
 
 mkdir /srv/vms
-VBoxManage createhd --filename /srv/vms/windowscuckoo.vdi --size 10000
-VBoxManage storagectl "Windowscuckoo" --name "IDE Controller" --add ide
-VBoxManage storageattach "Windowscuckoo" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium /srv/vms/windowscuckoo.vd1
+VBoxManage createhd --filename /srv/vms/cuckoo1.vdi --size 10000
+VBoxManage storagectl "cuckoo1" --name "IDE Controller" --add ide
+VBoxManage storageattach "cuckoo1" --storagectl "IDE Controller" --port 0 --device 0 --type hdd --medium /srv/vms/cuckoo1.vd1
 
 download
 
-VBoxManage storageattach "Windowscuckoo" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium /srv/vms/windowscuckoo.iso
+VBoxManage storageattach "cuckoo1" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium /srv/vms/cuckoo1.iso
 echo -e  "${green}[*]Running Windows VM${nc}"
-VBoxManage modifyvm "Windowscuckoo" --vrde on
+VBoxManage modifyvm "cuckoo1" --vrde on
 if [$?>0]
 then
     echo -e "${red}[!]VM FAILED${nc}"
