b35f9771df419e7f1157df70a45fc5ea679d21e3
kmsheldon <kms8085@rit.edu>
2017-11-25 16:24:55 -0500

    Delete verify_file.py
    No longer in use

--- verify_file.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-	params = {'apikey': '<YOUR-API-KEY>', 'resource': resource}
-	headers = {
-	  "Accept-Encoding": "gzip, deflate",
-	  "User-Agent" : "gzip,  My Python requests library example client or username"
-	  }
-	response = requests.post('https://www.virustotal.com/vtapi/v2/file/rescan',params=params)
-	json_response = response.json()
